01da3fd52f67a85d44e1721af690513ba8e416af
Well-Legend <a0970378051@gmail.com>
2023-02-19 15:22:55 +0800

    Compile, Migrate, and Test Contract

--- test.js
+++ test.js
@@ -11,25 +11,24 @@ const address = '0xc5f7f02e4833F2d8FddA9cD51E720793583B5A7a'; //user
 const password = 'well1314'
 let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(ethereumUri));
+// if(!web3.isConnected()){
+//     throw new Error('unable to connect to ethereum node at ' + ethereumUri);
+// }else{
+//     console.log('connected to ehterum node at ' + ethereumUri);
+//     let coinbase = web3.eth.coinbase;
+//     console.log('coinbase:' + coinbase);
+//     let balance = web3.eth.getBalance(coinbase);
+//     console.log('balance:' + web3.fromWei(balance, 'ether') + " ETH");
+//     let accounts = web3.eth.accounts;
+//     console.log(accounts);
 
-if(!web3.isConnected()){
-    throw new Error('unable to connect to ethereum node at ' + ethereumUri);
-}else{
-    console.log('connected to ehterum node at ' + ethereumUri);
-    let coinbase = web3.eth.coinbase;
-    console.log('coinbase:' + coinbase);
-    let balance = web3.eth.getBalance(coinbase);
-    console.log('balance:' + web3.fromWei(balance, 'ether') + " ETH");
-    let accounts = web3.eth.accounts;
-    console.log(accounts);
-
-    //部署時解鎖帳戶
-    if(web3.personal.unlockAccount(address, password)){
-        console.log(`${address} is unlocked`);
-    }else{
-        console.log(`unlock failed, ${address}`);
-    }
-}
+//     //部署時解鎖帳戶
+//     if(web3.personal.unlockAccount(address, password)){
+//         console.log(`${address} is unlocked`);
+//     }else{
+//         console.log(`unlock failed, ${address}`);
+//     }
+// }
 
 //input the contract
 const source = fs.readFileSync('./contracts/Note.sol', 'utf8');
@@ -61,13 +60,11 @@ const abi = contractFile.abi;
 //console.log(abi);
 
 //test function in contract
-const contract = web3.eth.contract(abi, '0xba2b6c297b0b2ce1b0837b2301803b858cff62c4');
-console.log(contract);
-contract.methods.addNote(
-    'Well'
-).send({
-    from: accounts[0],
-    gas: 100000000
+const contract = new web3.eth.Contract(abi, '0xba2b6c297b0b2ce1b0837b2301803b858cff62c4');
+//調用函式所發起的合約
+contract.methods.getNotesLen(address).send({
+    from: address,
+    gas: 1000000
     }, function (error, transactionHash) {
       console.log(error, transactionHash)
     }).on('error', function (error) {
@@ -78,4 +75,14 @@ contract.methods.addNote(
       console.log("YA", receipt) // contains the new contract address
     });
     console.log("success");
+//查看函式回傳值
+contract.methods.getNotesLen(address).call()
+.then(console.log);
 
+// //回調合約
+// web3.eth.getBlock(8, function(error, result){
+//     if(!error)
+//         console.log(JSON.stringify(result));
+//     else
+//         console.error(error);
+// })
